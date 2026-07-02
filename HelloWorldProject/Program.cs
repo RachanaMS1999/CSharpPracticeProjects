19f71b9a8b8b59f7ee6df5105f4960e1bcd0bb5f
RachanaMS1999 <rachanams79@gmail.com>
2026-07-03 00:44:16 +0530

    Changes are commited

--- HelloWorldProject/Program.cs
+++ HelloWorldProject/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorldProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Rachana");
+            Console.WriteLine("Hello, Rachu");
             Console.ReadLine();
         }
     }
